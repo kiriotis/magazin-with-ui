ef215dce0ead46f19908da4473a819a461069c76
ivan <ivan@DESKTOP-NELLSAH>
2022-05-01 21:12:58 +0300

    feat : json from web client

--- magazin with ui/json_provaider.cs
+++ magazin with ui/json_provaider.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Text.Json;
 using Newtonsoft.Json;
+using System.Net;
+
 
 
 namespace magazin_with_ui
@@ -16,10 +18,14 @@ namespace magazin_with_ui
         
         public void provide()
         {
-            
+            WebClient client = new WebClient();
+            Stream stream = client.OpenRead("https://raw.githubusercontent.com/kiriotis/magazin-with-ui/master/magazin%20with%20ui/Resources/products.json");
+            StreamReader reader = new StreamReader(stream);
+            var jsonFile = reader.ReadToEnd();
+
             items count_items = new items();
-            var path = "products.json";
-            string jsonFile = File.ReadAllText(path);
+           // var path =;
+           // var jsonFile = File.ReadAllText(path);
             Json_items json_ = JsonConvert.DeserializeObject<Json_items>(jsonFile);
             var res = JsonConvert.DeserializeObject<Dictionary<int,Json_items>>(jsonFile);
 
